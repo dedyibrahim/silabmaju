46f60072c0c215ed10ca218a437619cb546d9474
Dedy ibrahim <dedyibrahym23@gmail.com>
2019-07-10 15:58:27 +0700

    first commit

---
+++ application/views/umum/V_data.php
@@ -0,0 +1,29 @@
+  <div class="row">
+                   
+                    <!-- Column -->
+                    <div class="col-md-4 col-lg-4 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
+                                <h6 class="text-white">Forms</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-4 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
+                                <h6 class="text-white">Forms</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-lg-4 col-xlg-3">
+                        <div class="card card-hover">
+                            <div class="box bg-danger text-center">
+                                <h1 class="font-light text-white"><i class="mdi mdi-receipt"></i></h1>
+                                <h6 class="text-white">Forms</h6>
+                            </div>
+                        </div>
+                    </div>
+                    
+                </div>
---
+++ application/views/umum/V_header.php
@@ -0,0 +1,36 @@
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url() ?>assets/images/favicon.png">
+    <title>Matrix Template - The Ultimate Multipurpose admin template</title>
+    <link href="<?php echo base_url() ?>assets/libs/flot/css/float-chart.css" rel="stylesheet">
+    <link href="<?php echo base_url('assets/') ?>dist/css/style.min.css" rel="stylesheet">
+    <script src="<?php echo base_url() ?>assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/popper.js/dist/umd/popper.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/extra-libs/sparkline/sparkline.js"></script>
+    <script src="<?php echo base_url('assets/') ?>dist/js/waves.js"></script>
+    <script src="<?php echo base_url('assets/') ?>dist/js/sidebarmenu.js"></script>
+    <script src="<?php echo base_url('assets/') ?>dist/js/custom.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot/excanvas.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot/jquery.flot.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot/jquery.flot.pie.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot/jquery.flot.time.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot/jquery.flot.stack.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot/jquery.flot.crosshair.js"></script>
+    <script src="<?php echo base_url() ?>assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+    <script src="<?php echo base_url('assets/') ?>dist/js/pages/chart/chart-page-init.js"></script>
+ </head>
+
+<body>
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="lds-pos"></div>
+            <div class="lds-pos"></div>
+        </div>
+    </div>
